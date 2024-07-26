3ef2ca1ac134efe53c4213e8acad628c4ed678f6
Vinicius Fillos <vinifillos@gmail.com>
2024-07-26 12:06:58 -0300

    Project completed

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+include('conexao.php');
+
+if(isset($_POST['email']) || isset($_POST['senha'])) {
+
+    if(strlen($_POST['email']) == 0) {
+        echo "Preencha seu email";
+    } else if(strlen($_POST['senha']) == 0) {
+        echo "Preencha sua senha";
+    } else {
+
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
+
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+        $quantidade = $sql_query->num_rows;
+        
+        if($quantidade == 1) {
+
+            $usuario = $sql_query->fetch_assoc();
+
+            if(!isset($_SESSION)) {
+                session_start();
+            }
+
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            header("Location: painel.php");
+
+        } else {
+            echo "Falha ao logar! E-mail ou senha incorretos";
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login </title>
+</head>
+<body>
+    <h1>Acesse sua conta</h1>
+    <form action="" method="POST">
+        <p>
+            <label for="email">E-mail</label>
+            <input type="text" name='email' id="email">
+        </p>
+        <p>
+            <label for="senha">Senha</label>
+            <input type="password" name='senha' id="senha">
+        </p>
+        <p>
+            <button type="submit">Entrar</button>
+        </p>
+    </form>
+</body>
+</html>
